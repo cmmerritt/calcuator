a16d5ecbc2aa81147076d45731dff7d47946e5a9
cmmerritt <christiane.merritt@gmail.com>
2021-03-19 14:26:44 -0700

    add tests for math functions

--- test/example.test.js
+++ test/example.test.js
@@ -2,8 +2,9 @@
 // import { example } from '../example.js';
 
 const test = QUnit.test;
+import { add, subtract, multiply, divide } from '../utils.js';
 
-test('time to test a function', (expect) => {
+/* test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
@@ -14,5 +15,30 @@ test('time to test a function', (expect) => {
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+ */
+
+test('test add function', (expect) => {
+    const expected = 8;
+    const actual = add(5, 3);
+    expect.equal(actual, expected);
+});
+
+test('test subtract function', (expect) => {
+    const expected = 2;
+    const actual = subtract(5, 3);
+    expect.equal(actual, expected);
+});
+
+test('test multiply function', (expect) => {
+    const expected = 15;
+    const actual = multiply(5, 3);
+    expect.equal(actual, expected);
+});
+
+test('test divide function', (expect) => {
+    const expected = 2;
+    const actual = divide(6, 3);
     expect.equal(actual, expected);
 });
